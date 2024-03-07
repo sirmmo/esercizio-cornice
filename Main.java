549c1e97205f3685e4ac96e5c70182e7771a9e4d
Marco Montanari <sirmmo@gmail.com>
2024-03-07 20:09:31 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ public class Main
 	public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     System.out.println("Inserire due numeri: ");
-    int num1 = in.nextInt();
-    int num2 = in.nextInt();
-    for (int i=0; i<num1; i++) {
-        for (int j=0; j<num2; j++) {
-            System.out.print("*");
+    int num1 = in.nextInt(); // lettura primo intero
+    int num2 = in.nextInt(); // lettura secondo intero
+    for (int i=0; i<num1; i++) { // ciclo che definisce le righe (a partire dal primo intero)
+        for (int j=0; j<num2; j++) { // ciclo che definisce le colonne (a partire dal secondo intero)
+            System.out.print("*"); // stampa di asterisco
         }
-        System.out.println("");
+        System.out.println(""); // stampa dell simbolo di a capo alla fine della riga
     }
 
     }
